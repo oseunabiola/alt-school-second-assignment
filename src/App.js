bb126529f21ebaa968aae33feb74fc4adb6f470f
Oluwaseun Abiola <oluabiolaseun@gmail.com>
2022-10-30 13:26:11 +0100

    ADD: Client-side user pagination

--- src/App.js
+++ src/App.js
@@ -7,11 +7,8 @@ export default function App() {
   return (
     <ErrorBoundary>
       <Router>
-        <div style={{ height: "100%" }}>
-          <div
-            className="d-flex flex-column justify-content-center align-items-center"
-            style={{ height: "100%" }}
-          >
+        <div className="container py-3">
+          <div className="d-flex flex-column justify-content-center align-items-center">
             <Nav />
             <Routes>
               <Route index path="/" element={<Home />}></Route>
@@ -32,36 +29,43 @@ export default function App() {
 
 function Nav() {
   return (
-    <nav>
-      <div className="container">
-        <ul>
-          <li className="nav-link">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="nav-link">
-            <Link to="/">Home</Link>
-          </li>
-        </ul>
-      </div>
+    <nav className="mb-3">
+      <ul className="d-flex">
+        <li className="nav-link">
+          <Link to="/">Home</Link>
+        </li>
+        <li className="nav-link">
+          <Link to="/users">Users</Link>
+        </li>
+        <li className="nav-link">
+          <Link to="/nested">Nested</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
 
 function NestedRouteOne() {
-  return <div>I am nested route 1 mnkl</div>;
+  return <div>I am nested route 1</div>;
 }
 function NestedRouteTwo() {
   return <div>I am nested route 2</div>;
 }
 function NestedRouteParent() {
   return (
-    <div>
+    <>
       <nav>
-        <Link to="route-one">Route 1</Link>
-        <Link to="route-two">Route 2</Link>
+        <ul className="d-flex">
+          <li className="nav-link">
+            <Link to="route-one">Route 1</Link>
+          </li>
+          <li className="nav-link">
+            <Link to="route-two">Route 2</Link>
+          </li>
+        </ul>
       </nav>
       <Outlet />
-    </div>
+    </>
   );
 }
 function PageNotFound() {
